a1fa42c58c6200036a24aa0225037949a88c7771
ptone <preston@ptone.com>
2011-03-29 22:52:16 -0700

    moved to simplified method of getting git user info

--- django_paster/pastertemplates.py
+++ django_paster/pastertemplates.py
@@ -1,3 +1,4 @@
+from datetime import date
 import os
 import shutil
 import ConfigParser
@@ -10,16 +11,6 @@ from paste.script.templates import Template, var
 def sh(cmd):
     return Popen(cmd,shell=True,stdout=PIPE,stderr=PIPE).communicate()[0]
 
-def get_git_user():
-    # @@ also can just do sh('git config --get user.name')
-    # os.path.expanduser(path)
-    path = '/Users/preston/.gitconfig'
-    fixed_string = '\n'.join([x.lstrip() for x in open(path)])
-    stringobj = StringIO.StringIO(fixed_string)
-    config = ConfigParser.SafeConfigParser()
-    config.readfp(stringobj)
-    return dict(config.items('user'))['name']
-
 class DjangoAppTemplate(Template):
     _template_dir = 'templates/app_package'
     summary = 'template for a distributable django app'
@@ -28,10 +19,11 @@ class DjangoAppTemplate(Template):
         var('description', 'One-line description of the package'),
         var('long_description', 'Multi-line description (in reST)'),
         var('keywords', 'Space-separated keywords/tags'),
-        var('author', 'Author name', default=get_git_user()),
-        var('author_email', 'Author email'),
+        var('author', 'Author name', default=sh('git config --get user.name')),
+        var('author_email', 'Author email', default=sh('git config --get user.email')),
         var('url', 'URL of homepage'),
         var('license_name', 'License name', default="BSD"),
+        var('current_year', 'Copyright year', default=date.today().year)
         # var('zip_safe', 'True/False: if the package can be distributed as a .zip file',
             # default=False),
     ]
